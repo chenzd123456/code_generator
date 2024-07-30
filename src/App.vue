ee6e4e375f2b5d5cc4b80abb105f7a6872eaab64
Adam <chenzd123456@gmail.com>
2024-07-30 14:52:53 +0800

    generate the code automatically when meta or template changed

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,6 @@
   <div>
     <el-container class="fullscreen">
       <el-main>
-        <el-row>
-          <el-col>
-            <el-button @click="handleClick">gen</el-button>
-          </el-col>
-        </el-row>
         <el-row :gutter="20" style="height: 100%">
           <el-col :span="8"><CodeEditor language="yaml" v-model="meta" /></el-col>
           <el-col :span="8"><CodeEditor language="twig" v-model="template" /></el-col>
@@ -24,25 +19,22 @@ import { computed, ref } from 'vue'
 import nunjucks from 'nunjucks'
 import yaml from 'js-yaml'
 import CodeEditor from '@/components/CodeEditor.vue'
-// const input = reactive({ meta: '', template: '' })
+
 const meta = ref('')
 const template = ref('')
-const code = ref('')
-// const code = computed(() => {
-//   console.log(yaml.load(meta.value))
-//   console.log(template.value)
-//   const text = nunjucks.renderString(template.value, yaml.load(meta.value))
-//   console.log(text)
-//   return text
-// })
-const handleClick = () => {
-  const meta_data = yaml.load(meta.value)
-  console.log(JSON.stringify(meta_data))
-  console.log(template.value)
-  const text = nunjucks.renderString(template.value, meta_data)
-  console.log(text)
-  code.value = text
-}
+
+const code = computed(() => {
+  try {
+    console.log(meta.value)
+    console.log(template.value)
+    const meta_data = yaml.load(meta.value)
+    const text = nunjucks.renderString(template.value, meta_data)
+    return text
+  } catch (err) {
+    console.log(err)
+    return ''
+  }
+})
 </script>
 
 <style scoped></style>
